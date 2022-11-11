63d4af39e9322a11d0a45be9fcb7466f89257c3e
amd-boy-no-ne-tvoi <105717927+amd-boy-no-ne-tvoi@users.noreply.github.com>
2022-11-11 16:44:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+Stackdriver Error Reporting transport for Winston logger
+========================================================
+
+[![Build Status](https://travis-ci.org/findanyemail/winston-transport-stackdriver-error-reporting.svg?branch=master)](https://travis-ci.org/findanyemail/winston-transport-stackdriver-error-reporting "Build Status")
+[![Code Climate](https://codeclimate.com/github/findanyemail/winston-transport-stackdriver-error-reporting/badges/gpa.svg)](https://codeclimate.com/github/findanyemail/winston-transport-stackdriver-error-reporting "Code Climate")
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/findanyemail/winston-transport-stackdriver-error-reporting/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/findanyemail/winston-transport-stackdriver-error-reporting/?branch=master "Scrutinizer Code Quality")
+[![NPM Version](https://img.shields.io/npm/v/@findanyemail/winston-transport-stackdriver-error-reporting.svg)](https://www.npmjs.com/package/@findanyemail/winston-transport-stackdriver-error-reporting "NPM Version")
+
+Overview
+--------
+
+This package is [Winston] logger transport that sends errors to [Stackdriver Error Reporting] service.
+
+It's able to work in two modes:
+
+1. Logging to stdout compatible with [Google Container Engine];
+2. Logging via an API with a help of [@google-cloud/error-reporting] library.
+
+
+
+Usage and Configuration
+-----------------------
+
+Simply add this to your Winston transports array:
+
+```js
+    winston.configure({
+      transports: [
+        new StackdriverErrorReporting(options),
+      ],
+    });
+```
+
+This transport supports 100% same configuration options as [@google-cloud/error-reporting] library, and uses API transport by default.
+The only additional option is `options.level` that controls minimal log entry level that should be sent to Stackdriver.
+
+If you want to use stdout logging mode, all you need is to switch mode and set your `serviceContext`:
+
+```js
+    winston.configure({
+      transports: [
+        new StackdriverErrorReporting({
+          mode: 'console',
+          serviceContext: {
+            service: 'my service',
+            version: '1.0.0'
+          }
+        }),
+      ],
+    });
+```
+
+
+How it works
+------------
+
+When log entry reaches this transport, it will search `meta` object of log entry for objects that looks like errors
+(with `stack` property defined).
+After that, all errors will be delivered to [Stackdriver Error Reporting] service.
+
+If `meta.context` property is defined, it will be attached to error as it's context.
+Please see [Stackdriver Error Reporting documentation](https://cloud.google.com/error-reporting/docs/formatting-error-messages) for details.
+
+
+Alternatives
+------------
+
+Official package [@google-cloud/logging-winston] exists now, a winston transport for Stackdriver Logging, which can automatically report logged errors to Error Reporting.
+
+
+[//]: # (These are reference links used in the body of this document)
+
+[Winston]: <https://github.com/winstonjs/winston>
+[Google Container Engine]: <https://cloud.google.com/container-engine/>
+[Stackdriver Error Reporting]: <https://cloud.google.com/error-reporting/>
+[@google-cloud/error-reporting]: <https://www.npmjs.com/package/@google-cloud/error-reporting>
+[@google-cloud/logging-winston]: <https://www.npmjs.com/package/@google-cloud/logging-winston>
---
+++ src/index.js
@@ -0,0 +1,150 @@
+import forOwn from 'for-own'
+import isPlainObject from 'is-plain-object'
+import errorReporter from '@google-cloud/error-reporting'
+import {Transport} from 'winston/lib/winston/transports/transport'
+
+/**
+ * Winston transport for Stackdriver Error Reporting service.
+ */
+class StackdriverErrorReporting extends Transport {
+
+  /**
+   * Class constructor.
+   *
+   * @param {Object} options Configuration options.
+   */
+  constructor(options = {}) {
+
+    super(options);
+
+    this.name = 'StackdriverErrorReporting';
+    this.level = options.level || 'error';
+    this.mode = options.mode.toLowerCase() || 'api';
+    this.serviceContext = options.serviceContext || {service: 'unknown', version: 'unknown'};
+    if (this.mode !== 'console' && this.mode !== 'api') {
+      throw new Error('StackdriverErrorReporting: parameter "mode" is expected to be "console" or "api".');
+    }
+
+    if (this.mode === 'api') {
+      this._client = errorReporter(Object.assign({}, options, {
+        ignoreEnvironmentCheck: true,
+      }));
+    }
+  }
+
+  /**
+   * Send entry to Stackdriver Error Reporting.
+   *
+   * @param {String} level - Winston log entry level.
+   * @param {String} message - Log entry message.
+   * @param {Object} meta - Winston meta information.
+   * @param {Function} callback - Callback to let Winston know we are done.
+   */
+  log(level, message, meta, callback) {
+
+    const promise = (this.mode === 'api')
+      ? this.logApi(level, message, meta)
+      : this.logConsole(level, message, meta);
+
+    promise
+      .then(() => callback(null, true))
+      .catch((error) => callback(error, false));
+  }
+
+  /**
+   * Send entry to Stackdriver Error Reporting with a help of console.log().
+   *
+   * @param {String} level - Winston log entry level.
+   * @param {String} message - Log entry message.
+   * @param {Object} meta - Winston meta information.
+   * @return {Promise}
+   */
+  logConsole(level, message, meta) {
+
+    const eventTime = (new Date()).toISOString();
+    const errors = this.extractErrorsFromMeta(meta);
+    errors.forEach((error) => {
+
+      const stackTrace = Array.isArray(error.stack) ? error.stack.join("\n") : error.stack;
+      const message = JSON.stringify({
+        eventTime,
+        message: stackTrace,
+        severity: level.toUpperCase(),
+        serviceContext: this.serviceContext,
+      });
+      if (meta.context) {
+        message.context = meta.context;
+      }
+
+      console.log(message);
+    });
+
+    return Promise.resolve();
+  }
+
+  /**
+   * Send entry to Stackdriver Error Reporting with a help of API.
+   *
+   * @param {String} level - Winston log entry level.
+   * @param {String} message - Log entry message.
+   * @param {Object} meta - Winston meta information.
+   * @return {Promise}
+   */
+  logApi(level, message, meta) {
+
+    const promises = [];
+
+    const errors = this.extractErrorsFromMeta(meta);
+    errors.forEach((error) => {
+
+      const stackTrace = Array.isArray(error.stack) ? error.stack.join("\n") : error.stack;
+      promises.push(
+        new Promise((resolve, reject) => {
+          let request = null;
+          if (meta.context && meta.context.httpRequest) {
+            request = meta.context.httpRequest;
+          }
+          this._client.report(error, request, stackTrace, (reportError) => {
+            if (reportError) {
+              console.log('Failed to send error to Stackdriver Error Reporting.', reportError);
+              return reject(reportError);
+            }
+            return resolve();
+          })
+        })
+      );
+    });
+
+    return Promise.all(promises);
+  }
+
+  /**
+   * Extracts all errors from Winston metadata.
+   *
+   * @param {Object} data - Winston meta data.
+   * @return {Array}
+   */
+  extractErrorsFromMeta(data) {
+    if (!data) {
+      return [];
+    }
+
+    let errors = [];
+
+    if (data.stack) {
+      errors.push(data);
+    } else if (isPlainObject(data)) {
+      forOwn(data, (value) => {
+        errors = errors.concat(this.extractErrorsFromMeta(value));
+      });
+    } else if (Array.isArray(data)) {
+      data.forEach((value) => {
+        errors = errors.concat(this.extractErrorsFromMeta(value));
+      });
+    }
+
+    return errors;
+  }
+}
+
+export default StackdriverErrorReporting
